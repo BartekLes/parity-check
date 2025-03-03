c590c8417b59ee2546c1be28d010872ff3d5518d
Bartosz Lesiński <bartosz.lesinski@interia.pl>
2025-03-03 23:01:07 +0100

    Add project files

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parity_Check
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = GetNumber();
+            Console.WriteLine(number);
+
+            int GetNumber()
+            {
+                Console.WriteLine("Podaj liczbę");
+                var gettedNumber = int.Parse(Console.ReadLine());
+                return gettedNumber;
+            }
+
+        }
+    }
+}
