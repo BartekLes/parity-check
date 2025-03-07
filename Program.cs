5305abff2271d67e884368cb940157b99a9098fd
Bartosz Lesiński <bartosz.lesinski@interia.pl>
2025-03-07 23:33:46 +0100

    other method outside the main method

--- Program.cs
+++ Program.cs
@@ -12,29 +12,27 @@ namespace Parity_Check
         {
             var number = GetNumber();
             ParityCheck(number);
-
-            int GetNumber()
+        }
+        public static int GetNumber()
+        {
+            Console.WriteLine("Podaj liczbę.");
+            while (true)
             {
-                Console.WriteLine("Podaj liczbę.");
-                while (true)
+                if (!int.TryParse(Console.ReadLine(), out int input))
                 {
-                    if (!int.TryParse(Console.ReadLine(), out int input))
-                    {
-                        Console.WriteLine("Podana wartość nie jest liczbą. Spróbuj ponownie");
-                        continue;
-                    }
-
-                    return input;
+                    Console.WriteLine("Podana wartość nie jest liczbą. Spróbuj ponownie");
+                    continue;
                 }
+
+                return input;
             }
-            void ParityCheck (int numberToCheck)
-            {
-                    if (numberToCheck % 2 == 0)
-                        Console.WriteLine($"Liczba {numberToCheck} jest parzysta");
-                    else
-                        Console.WriteLine($"Liczba {numberToCheck} jest nieparzysta");
-            }
-        
+        }
+        public static void ParityCheck(int numberToCheck)
+        {
+            if (numberToCheck % 2 == 0)
+                Console.WriteLine($"Liczba {numberToCheck} jest parzysta");
+            else
+                Console.WriteLine($"Liczba {numberToCheck} jest nieparzysta");
         }
     }
 }
